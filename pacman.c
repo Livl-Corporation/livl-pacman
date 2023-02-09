e166ec6083a8e7cc332784d351de201eb26945e9
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-02-09 20:03:46 +0100

    feat: implement menu window and refactor name variables and methods

--- pacman.c
+++ pacman.c
@@ -1,89 +1,180 @@
 #include "pacman.h"
 
-void init()
+void initSDL()
 {
-	pWindow = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	win_surf = SDL_GetWindowSurface(pWindow);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        SDL_ExitWithError("Initialisation of the SDL");
 
-	plancheSprites = SDL_LoadBMP(PACMAN_PATH_BMP);
-    count = 0;
+    pWindow = SDL_CreateWindow(GAME_TITLE,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_SCREEN_WIDTH,WINDOW_SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+    if (pWindow == NULL)
+        SDL_ExitWithError("Creation of the SDL window");
+
+    pSurfaceWindow = SDL_GetWindowSurface(pWindow);
+    if(pSurfaceWindow == NULL)
+        SDL_ExitWithError("Creation of the SDL window surface");
+}
+
+void loadMedia()
+{
+    pSurfacePacmanSpriteSheet = SDL_LoadBMP(PATH_SPRITE_SHEET_IMAGE);
+    if(pSurfacePacmanSpriteSheet == NULL)
+        SDL_ExitWithError("Loading of the sprite-sheet");
+
+    pSurfacePlayButton = SDL_LoadBMP(PATH_PLAY_BTN_IMAGE);
+    if (pSurfacePlayButton == NULL)
+        SDL_ExitWithError("Loading of the button play");
+}
+
+void displayMainMenu()
+{
+    SDL_SetColorKey(pSurfacePacmanSpriteSheet, true, 0);
+    SDL_BlitScaled(pSurfacePacmanSpriteSheet, &imgBlackBackground, pSurfaceWindow, &imgBlackBackground);
+    SDL_BlitScaled(pSurfacePacmanSpriteSheet, &imgPacmanTitle, pSurfaceWindow, &imgPacmanTitleZoom);
+
+
+    SDL_SetColorKey(pSurfacePlayButton, true, 0);
+    SDL_BlitScaled(pSurfacePlayButton, &imgPlayButton, pSurfaceWindow, &imgPlayButtonZoom);
+
+    SDL_UpdateWindowSurface(pWindow);
+
+    handleMainMenuEvents();
+}
+
+void handleMainMenuEvents()
+{
+    bool quit = false;
+    int x = 0, y = 0;
+
+    while (!quit) // Wait for an event
+    {
+        SDL_Event event;
+        while (!quit && SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    SDL_GetMouseState(&x, &y);
+                    if (isPointInPlayButton(x, y))
+                    {
+                        startGameLoop();
+                    }
+                    break;
+                default: break;
+            }
+        }
+    }
+}
+
+bool isPointInPlayButton(int x, int y)
+{
+    return x >= imgPlayButtonZoom.x && x <= imgPlayButtonZoom.x + imgPlayButtonZoom.w
+           && y >= imgPlayButtonZoom.y && y <= imgPlayButtonZoom.y + imgPlayButtonZoom.h;
+}
+
+bool handleGameEvents()
+{
+    SDL_Event event;
+    while (!pGameQuit && SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                *pGameQuit = true;
+                break;
+        }
+    }
+
+    int numberOfKeyboardScancodes;
+    const Uint8* keys = SDL_GetKeyboardState(&numberOfKeyboardScancodes);
+
+    if (keys[SDL_SCANCODE_ESCAPE])
+        *pGameQuit = true;
+    if (keys[SDL_SCANCODE_LEFT])
+        SDL_Log("LEFT");
+    if (keys[SDL_SCANCODE_RIGHT])
+        SDL_Log("RIGHT");
+    if (keys[SDL_SCANCODE_UP])
+        SDL_Log("UP");
+    if (keys[SDL_SCANCODE_DOWN])
+        SDL_Log("DOWN");
 }
 
 
+void startGameLoop()
+{
+    while (!pGameQuit)
+    {
+        handleGameEvents();
+
+        draw();
+
+        SDL_Delay(20); // ~50 fps use SDL_GetTicks64() pour plus de precision
+
+        SDL_UpdateWindowSurface(pWindow);
+    }
+}
+
 void draw()
 {
-    SDL_SetColorKey(plancheSprites, false, 0);
-    SDL_BlitScaled(plancheSprites, &src_bg, win_surf, &bg);
+    SDL_SetColorKey(pSurfacePacmanSpriteSheet, false, 0);
+    SDL_BlitScaled(pSurfacePacmanSpriteSheet, &imgMazeBlueCoins, pSurfaceWindow, &imgMazeBlueCoinsZoom);
 
-    SDL_Rect* ghost_in = NULL;
+    SDL_Rect* newGhost = NULL;
     switch (count/128)
     {
         case 0:
-            ghost_in = &(ghost_r);
-            ghost.x++;
+            newGhost = &(ghostRedEyesRight);
+            ghostRedZoom.x++;
             break;
         case 1:
-            ghost_in = &(ghost_d);
-            ghost.y++;
+            newGhost = &(ghostRedEyesDown);
+            ghostRedZoom.y++;
             break;
         case 2:
-            ghost_in = &(ghost_l);
-            ghost.x--;
+            newGhost = &(ghostRedEyesLeft);
+            ghostRedZoom.x--;
             break;
         case 3:
-            ghost_in = &(ghost_u);
-            ghost.y--;
+            newGhost = &(ghostRedEyesUp);
+            ghostRedZoom.y--;
             break;
     }
     count =(count+1)%(512);
 
-    SDL_Rect ghost_in2 = *ghost_in;
+    SDL_Rect ghost_in2 = *newGhost;
     if ((count/4)%2)
         ghost_in2.x += 17;
-        
-    SDL_SetColorKey(plancheSprites, true, 0);
-	SDL_BlitScaled(plancheSprites, &ghost_in2, win_surf, &ghost);
+
+    SDL_SetColorKey(pSurfacePacmanSpriteSheet, true, 0);
+    SDL_BlitScaled(pSurfacePacmanSpriteSheet, &ghost_in2, pSurfaceWindow, &ghostRedZoom);
 }
 
-
-int main(int argc, char** argv)
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char** argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-    {
-		fprintf(stderr,"Echec de l'initialisation de la SDL %s",SDL_GetError());
-       	return 1;
-    }
+    initSDL();
 
-	init();
-    
-	bool quit = false;
-	while (!quit)
-	{
-		SDL_Event event;
-		while (!quit && SDL_PollEvent(&event))
-		{
-			switch (event.type)
-			{
-			case SDL_QUIT:
-				quit = true;
-				break;
-			default: break;
-			}
-		}
-        int nbk;
-        const Uint8* keys = SDL_GetKeyboardState(&nbk);
-        if (keys[SDL_SCANCODE_ESCAPE])
-            quit = true;
-        if (keys[SDL_SCANCODE_LEFT])
-            puts("LEFT");
-        if (keys[SDL_SCANCODE_RIGHT])
-            puts("RIGHT");
+    loadMedia();
 
-		draw();
+    displayMainMenu();
 
-        SDL_Delay(20); // ~50 fps use SDL_GetTicks64() pour plus de precision
-		SDL_UpdateWindowSurface(pWindow);
-	}
-    SDL_Quit();
+    closeWindow();
+
     return 0;
+}
+
+void SDL_ExitWithError(const char *message)
+{
+    SDL_Log("ERROR : %s > %s\n", message, SDL_GetError());
+    SDL_Quit();
+    exit(EXIT_FAILURE);
+}
+
+void closeWindow()
+{
+    SDL_FreeSurface(pSurfacePacmanSpriteSheet);
+    SDL_FreeSurface(pSurfacePlayButton);
+    SDL_FreeSurface(pSurfaceWindow);
+    SDL_DestroyWindow(pWindow);
+    SDL_Quit();
 }
--- pacman.h
+++ pacman.h
@@ -6,30 +6,58 @@
 #include <stdbool.h>
 
 #define GAME_TITLE "PacMan"
-#define PACMAN_PATH_BMP "./pacman_sprites.bmp"
-#define GAME_SCREEN_WIDTH 700
-#define GAME_SCREEN_HEIGHT 900
-
-SDL_Window* pWindow = NULL;
-SDL_Surface* win_surf = NULL;
-SDL_Surface* plancheSprites = NULL;
-
-SDL_Rect src_bg = { 200,3, 168,216 }; // x,y, w,h (0,0) en haut a gauche
-SDL_Rect bg = { 4,4, 672,864 }; // ici scale x4
-
-SDL_Rect ghost_r = { 3,123, 16,16 };
-SDL_Rect ghost_l = { 37,123, 16,16 };
-SDL_Rect ghost_d = { 105,123, 16,16 };
-SDL_Rect ghost_u = { 71,123, 16,16 };
-SDL_Rect ghost = { 34,34, 32,32 };     // ici scale x2
-
-int count;
-
-void init();
+#define PATH_SPRITE_SHEET_IMAGE "../assets/img/pacman_sprites.bmp"
+#define PATH_PLAY_BTN_IMAGE "../assets/img/play_btn.bmp"
+#define WINDOW_SCREEN_WIDTH 700
+#define WINDOW_SCREEN_HEIGHT 900
 
 /**
- * @brief Met à jour la surface de la fenetre "win_surf"
- */
+ * VARIABLES
+**/
+int count = 0;
+bool* pGameQuit = false;
+SDL_Window* pWindow = NULL;
+SDL_Surface* pSurfaceWindow = NULL;
+SDL_Surface* pSurfacePacmanSpriteSheet = NULL;
+SDL_Surface* pSurfacePlayButton = NULL;
+
+/**
+ * IMAGE : Spire-Sheets
+**/
+SDL_Rect imgMazeBlueCoins = {200, 3, 168, 216 };
+SDL_Rect imgMazeBlueCoinsZoom = {4, 4, 672, 864 }; // we zoom the maze x4 to fit the window
+
+SDL_Rect ghostRedEyesRight = {3, 123, 16, 16 };
+SDL_Rect ghostRedEyesLeft = {37, 123, 16, 16 };
+SDL_Rect ghostRedEyesDown = {105, 123, 16, 16 };
+SDL_Rect ghostRedEyesUp = {71, 123, 16, 16 };
+SDL_Rect ghostRedZoom = {34, 34, 32, 32 }; // we zoom the ghost x2
+
+SDL_Rect imgPacmanTitle = {0, 0, 190, 50 };
+SDL_Rect imgPacmanTitleZoom = {100, 100, 500, 130 };
+SDL_Rect imgBlackBackground = {0, 0, 0, 0 };
+
+/**
+ * IMAGE : Play button
+**/
+SDL_Rect imgPlayButton = {0, 0, 98, 28 };
+SDL_Rect imgPlayButtonZoom = {200, 400, 300, 100 };
+
+/**
+ * FUNCTIONS
+**/
+void initSDL();
+void loadMedia();
+
+void displayMainMenu();
+void handleMainMenuEvents();
+bool isPointInPlayButton(int x, int y);
+
+void startGameLoop();
+bool handleGameEvents();
 void draw();
+
+void SDL_ExitWithError(const char *message);
+void closeWindow();
 
 #endif // PACMAN_H
