5a911217eef812ea6e1e9f435d1c6d7d422de4a7
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-02-19 14:45:46 +0100

    feat: implementation of detection of collision from the pacman movement

--- src/ui/game_window.c
+++ src/ui/game_window.c
@@ -1,7 +1,9 @@
 #include "game_window.h"
 
 int count = 0;
-bool* pGameQuit = false;
+
+bool pGameQuit = false;
+MovementDirection actualPacmanMovementDirection = MOVEMENT_DIRECTION_RIGHT;
 
 SDL_Rect imgMazeBlueCoins = {200, 3, 168, 216 };
 SDL_Rect imgMazeBlueCoinsZoom = {4, 4, 672, 864 };
@@ -10,10 +12,24 @@ SDL_Rect ghostRedEyesRight = {3, 123, 16, 16 };
 SDL_Rect ghostRedEyesLeft = {37, 123, 16, 16 };
 SDL_Rect ghostRedEyesDown = {105, 123, 16, 16 };
 SDL_Rect ghostRedEyesUp = {71, 123, 16, 16 };
-SDL_Rect ghostRedZoom = {34, 34, 32, 32 };
+SDL_Rect ghostRedZoom = {34, 34, 36, 36 };
+
+void initLoadMaze()
+{
+    if(pacmanMazeArray == NULL)
+    {
+        if(!retrieveMazeFromFile())
+        {
+            ConsoleHandlerDisplayError("while retrieving maze from file.");
+            pGameQuit = true;
+        }
+    }
+}
 
 void startGameLoop()
 {
+    initLoadMaze();
+
     while (!pGameQuit)
     {
         handleGameEvents();
@@ -24,15 +40,35 @@ void startGameLoop()
 
         SDL_UpdateWindowSurface(pWindow);
     }
+
+
+    freeMaze();
+}
+
+MovementDirection getMovementDirectionFromKey(SDL_Keycode key)
+{
+    switch (key) {
+        case SDLK_LEFT:
+            return MOVEMENT_DIRECTION_LEFT;
+        case SDLK_RIGHT:
+            return MOVEMENT_DIRECTION_RIGHT;
+        case SDLK_UP:
+            return MOVEMENT_DIRECTION_UP;
+        case SDLK_DOWN:
+            return MOVEMENT_DIRECTION_DOWN;
+        default:
+            return -1;
+    }
 }
 
 bool handleGameEvents()
 {
     SDL_Event event;
+
     while (!pGameQuit && SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_QUIT:
-                *pGameQuit = true;
+                pGameQuit = true;
                 break;
         }
     }
@@ -40,8 +76,10 @@ bool handleGameEvents()
     int numberOfKeyboardScancodes;
     const Uint8* keys = SDL_GetKeyboardState(&numberOfKeyboardScancodes);
 
+    //actualPacmanMovementDirection = getMovementDirectionFromKey(event.key.keysym.sym);
+
     if (keys[SDL_SCANCODE_ESCAPE])
-        *pGameQuit = true;
+        pGameQuit = true;
     if (keys[SDL_SCANCODE_LEFT])
         SDL_Log("LEFT");
     if (keys[SDL_SCANCODE_RIGHT])
@@ -50,6 +88,30 @@ bool handleGameEvents()
         SDL_Log("UP");
     if (keys[SDL_SCANCODE_DOWN])
         SDL_Log("DOWN");
+}
+
+SDL_Point getPacmanPosition(const SDL_Rect pacman)
+{
+    SDL_Point position;
+
+    if (actualPacmanMovementDirection == MOVEMENT_DIRECTION_LEFT) {
+        position.x = pacman.x - 1;
+        position.y = pacman.y + pacman.h / 2;
+    } else if (actualPacmanMovementDirection == MOVEMENT_DIRECTION_RIGHT) {
+        position.x = pacman.x + pacman.w + 1;
+        position.y = pacman.y + pacman.h / 2;
+    } else if (actualPacmanMovementDirection == MOVEMENT_DIRECTION_UP) {
+        position.x = pacman.x + pacman.w  / 2;
+        position.y = pacman.y - 1;
+    } else if (actualPacmanMovementDirection == MOVEMENT_DIRECTION_DOWN) {
+        position.x = pacman.x + pacman.w  / 2;
+        position.y = pacman.y + pacman.h + 1;
+    }
+
+    position.x = position.x / SQUARE_WIDTH;
+    position.y = position.y / SQUARE_HEIGHT;
+
+    return position;
 }
 
 void drawGame()
@@ -62,26 +124,31 @@ void drawGame()
     {
         case 0:
             newGhost = &(ghostRedEyesRight);
+            actualPacmanMovementDirection = MOVEMENT_DIRECTION_RIGHT;
             ghostRedZoom.x++;
             break;
         case 1:
             newGhost = &(ghostRedEyesDown);
+            actualPacmanMovementDirection = MOVEMENT_DIRECTION_DOWN;
             ghostRedZoom.y++;
             break;
         case 2:
             newGhost = &(ghostRedEyesLeft);
+            actualPacmanMovementDirection = MOVEMENT_DIRECTION_LEFT;
             ghostRedZoom.x--;
             break;
         case 3:
             newGhost = &(ghostRedEyesUp);
+            actualPacmanMovementDirection = MOVEMENT_DIRECTION_UP;
             ghostRedZoom.y--;
             break;
     }
-    count =(count+1)%(512);
 
     SDL_Rect ghost_in2 = *newGhost;
-    if ((count/4)%2)
-        ghost_in2.x += 17;
+
+    SDL_Point pacmanPosition = getPacmanPosition(ghostRedZoom);
+    MazeElement mazeElement = getCoordinateMazeElement(pacmanPosition.y, pacmanPosition.x);
+    ConsoleHandlerDisplayMessage("row %d - col %d - maze '%c'", pacmanPosition.y, pacmanPosition.x, (char)mazeElement);
 
     SDL_SetColorKey(pSurfacePacmanSpriteSheet, true, 0);
     SDL_BlitScaled(pSurfacePacmanSpriteSheet, &ghost_in2, pSurfaceWindow, &ghostRedZoom);
--- src/ui/game_window.h
+++ src/ui/game_window.h
@@ -7,14 +7,29 @@
 
 #include <stdbool.h>
 #include <SDL.h>
-
+#include "../models/maze.h"
+#include "../utilities/console_handler.h"
 #include "variables.h"
 
-extern int count;
-extern bool* pGameQuit;
+#define SQUARE_WIDTH 32
+#define SQUARE_HEIGHT 32
 
+typedef enum {
+    MOVEMENT_DIRECTION_LEFT = 0,
+    MOVEMENT_DIRECTION_RIGHT = 1,
+    MOVEMENT_DIRECTION_UP = 2,
+    MOVEMENT_DIRECTION_DOWN = 3,
+} MovementDirection;
+
+extern int count;
+extern bool pGameQuit;
+extern MovementDirection actualPacmanMovementDirection;
+
+MovementDirection getMovementDirectionFromKey(SDL_Keycode key);
+void initLoadMaze();
 void startGameLoop();
 bool handleGameEvents();
+SDL_Point getPacmanPosition(SDL_Rect pacman);
 void drawGame();
 
 #endif //PACMAN_GAME_WINDOW_H
