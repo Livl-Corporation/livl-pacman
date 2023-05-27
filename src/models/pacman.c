f41379a2ff8bd221ee415e52446b16bbc7746422
Franck G <gutmann.franck@outlook.fr>
2023-05-27 18:30:41 +0200

    feat: use wasd keys to move pacman

--- src/models/pacman.c
+++ src/models/pacman.c
@@ -30,7 +30,6 @@ void spawnPacman()
     pacmanUIPos = gridPosToUiPos(pacmanGridPos);
 
     pacmanDirection = DIRECTION_RIGHT;
-
 }
 
 void handlePacmanEvents()
@@ -39,15 +38,14 @@ void handlePacmanEvents()
     int numberOfKeyboardScancodes;
     const Uint8 *keys = SDL_GetKeyboardState(&numberOfKeyboardScancodes);
 
-    if (keys[SDL_SCANCODE_LEFT])
+    if (keys[SDL_SCANCODE_LEFT] || keys[SDL_SCANCODE_A])
         pacmanDirection = DIRECTION_LEFT;
-    if (keys[SDL_SCANCODE_RIGHT])
+    if (keys[SDL_SCANCODE_RIGHT] || keys[SDL_SCANCODE_D])
         pacmanDirection = DIRECTION_RIGHT;
-    if (keys[SDL_SCANCODE_UP])
+    if (keys[SDL_SCANCODE_UP] || keys[SDL_SCANCODE_W])
         pacmanDirection = DIRECTION_UP;
-    if (keys[SDL_SCANCODE_DOWN])
+    if (keys[SDL_SCANCODE_DOWN] || keys[SDL_SCANCODE_S])
         pacmanDirection = DIRECTION_DOWN;
-
 }
 
 void drawPacman(int count)
@@ -64,19 +62,19 @@ void drawPacman(int count)
     {
     case DIRECTION_RIGHT:
         newPacman = (pacmanAnimation == 0) ? &pacmanRight1 : &pacmanRight2;
-        pacmanPosCopy.x ++;
+        pacmanPosCopy.x++;
         break;
     case DIRECTION_LEFT:
         newPacman = (pacmanAnimation == 0) ? &pacmanLeft1 : &pacmanLeft2;
-        pacmanPosCopy.x --;
+        pacmanPosCopy.x--;
         break;
     case DIRECTION_UP:
         newPacman = (pacmanAnimation == 0) ? &pacmanUp1 : &pacmanUp2;
-        pacmanPosCopy.y --;
+        pacmanPosCopy.y--;
         break;
     case DIRECTION_DOWN:
         newPacman = (pacmanAnimation == 0) ? &pacmanDown1 : &pacmanDown2;
-        pacmanPosCopy.y ++;
+        pacmanPosCopy.y++;
         break;
     }
 
@@ -96,23 +94,22 @@ void drawPacman(int count)
         // Pacman has moved in grid :
         pacmanGridPos = newPacmanGridPos;
         onPacmanGridMove();
-
     }
 
     // Move is valid, update pacman position
     pacmanUIPos = pacmanPosCopy;
 
     pacmanBlit(newPacman);
-
 }
 
-void onPacmanGridMove() {
+void onPacmanGridMove()
+{
 
     // It's here that we will check if pacman has eaten food or is in colission with a ghost
-
 }
 
-void pacmanBlit(SDL_Rect *srcRect) {
+void pacmanBlit(SDL_Rect *srcRect)
+{
     SDL_Rect rect = {pacmanUIPos.x, pacmanUIPos.y, CELL_SIZE, CELL_SIZE};
     SDL_SetColorKey(pSurfacePacmanSpriteSheet, 1, 0);
     SDL_BlitScaled(pSurfacePacmanSpriteSheet, srcRect, pSurfaceWindow, &rect);
