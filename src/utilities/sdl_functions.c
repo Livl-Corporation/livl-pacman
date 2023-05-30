488ba4eacf3e1ff362713a88ca6a8d150622f368
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-05-30 21:43:09 +0200

    refactor: migration from old sprite sheet to the new one

--- src/utilities/sdl_functions.c
+++ src/utilities/sdl_functions.c
@@ -24,10 +24,6 @@ void loadMedia()
     pSurfacePacmanSpriteSheet = SDL_LoadBMP(PATH_SPRITE_SHEET_PACMAN);
     if (pSurfacePacmanSpriteSheet == NULL)
         SDL_ExitWithError("Loading of the sprite-sheet");
-
-    pSurfacePlayButton = SDL_LoadBMP(PATH_SPRITE_SHEET_PLAY_BTN);
-    if (pSurfacePlayButton == NULL)
-        SDL_ExitWithError("Loading of the button play");
 }
 
 void SDL_ExitWithError(const char *message)
