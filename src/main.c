0820776ac0d043584a1950c3b11f757f18ef9c24
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-06-18 17:12:07 +0200

    feat(sdl-function): add method to init and load sounds from SDL_Mixer library

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include "utilities/sdl_functions.h"
 #include "ui/menu_window.h"
+#include "models/audio.h"
 
 int main(void)
 {
     initSDL();
+    initSDL_Mixer();
 
     loadMedia();
+    initAudio();
 
     startMenu();
 
