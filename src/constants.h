6ce39e9a9d08fbabc4cf5a2b9b65cdb7314e4c7c
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-06-22 14:47:38 +0200

    feat(SDL): add Livl Pacman Logo as an icon of the app

--- src/constants.h
+++ src/constants.h
@@ -22,6 +22,7 @@
 
 #define PATH_FILE_MAZE "../assets/text/maze.txt"
 #define PATH_SPRITE_SHEET_PACMAN "../assets/img/pacman_sprites.bmp"
+#define PATH_LOGO_LIVL_PACMAN "../assets/img/logo_livl_pacman.bmp"
 
 #define GAME_SPEED 200.0   // IN FPS, BIGGER IS FASTER
 #define ANIMATION_SPEED 14 // BIGGER IS SLOWER
